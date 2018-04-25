a7a3e264afd5b414ca5577cc367d555de55cdc5f
Alexandre Wanick <alexandre@wvieira.com>
2018-04-25 16:45:42 -0300

    Add files via upload

---
+++ gl/window.hpp
@@ -0,0 +1,28 @@
+class Window {
+private:
+	float _L, _R, _B, _T;
+public:
+	Window(float L = -1, float R = 1, float B = -1, float T = 1)
+	{
+		_L = L;
+		_R = R;
+		_B = B;
+		_T = T;
+	}
+	float getL(void)
+	{
+		return _L;
+	}
+	float getR(void)
+	{
+		return _R;
+	}
+	float getB(void)
+	{
+		return _B;
+	}
+	float getT(void)
+	{
+		return _T;
+	}
+};
